f34e437342da790fa9962bfd5b269c8c5a7aa34f
Nick Izbrodin <nizbrodin@gmail.com>
2024-08-09 16:41:36 +0100

    Implemented entities, transformation/projection/view matrices. Spinning cube.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,7 +1,10 @@
 package engineTester;
 
+import entities.Camera;
+import entities.Entity;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import models.RawModel;
@@ -14,38 +17,100 @@ public class MainGameLoop {
 
         DisplayManager.createDisplay();
         Loader loader = new Loader();
-        Renderer renderer = new Renderer();
         StaticShader shader = new StaticShader();
+        Renderer renderer = new Renderer(shader);
 
         float[] vertices = {
-                -0.5f, 0.5f, 0f,//v0
-                -0.5f, -0.5f, 0f,//v1
-                0.5f, -0.5f, 0f,//v2
-                0.5f, 0.5f, 0f,//v3
+                -0.5f,0.5f,0,
+                -0.5f,-0.5f,0,
+                0.5f,-0.5f,0,
+                0.5f,0.5f,0,
+
+                -0.5f,0.5f,1,
+                -0.5f,-0.5f,1,
+                0.5f,-0.5f,1,
+                0.5f,0.5f,1,
+
+                0.5f,0.5f,0,
+                0.5f,-0.5f,0,
+                0.5f,-0.5f,1,
+                0.5f,0.5f,1,
+
+                -0.5f,0.5f,0,
+                -0.5f,-0.5f,0,
+                -0.5f,-0.5f,1,
+                -0.5f,0.5f,1,
+
+                -0.5f,0.5f,1,
+                -0.5f,0.5f,0,
+                0.5f,0.5f,0,
+                0.5f,0.5f,1,
+
+                -0.5f,-0.5f,1,
+                -0.5f,-0.5f,0,
+                0.5f,-0.5f,0,
+                0.5f,-0.5f,1
+
         };
 
         int[] indices = {
-                0,1,3, //top left triangle (v0, v1, v3)
-                3,1,2 //bottom right triangle (v3, v1, v2)
+                0,1,3,
+                3,1,2,
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
         };
 
         float[] textureCoords = {
-                0,0, //v1
-                0,1, //v2
-                1,1, //v3
-                1,0 //v4
+
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0
         };
 
         RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
         ModelTexture texture = new ModelTexture(loader.loadTexture("avatar"));
         TexturedModel texturedModel = new TexturedModel(model, texture);
 
+        Entity entity = new Entity(texturedModel, new Vector3f(0,0,-5), 0, 0, 0, 1);
+
+        Camera camera = new Camera();
+
         while (!Display.isCloseRequested()) {
-            //game logic
-            //render
+            entity.increaseRotation(0.5f,0.5f,0);
+            camera.move();
             renderer.prepare();
             shader.start();
-            renderer.render(texturedModel);
+            shader.loadViewMatrix(camera);
+            renderer.render(entity, shader);
             shader.stop();
             DisplayManager.updateDisplay();
         }
